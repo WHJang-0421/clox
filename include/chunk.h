f501824edb29d466918faddf9a49b6bccef7a920
WoohyukJang <morrranii@gmail.com>
2023-09-30 23:13:14 +0900

    Chapter 21 - Global Variables

--- include/chunk.h
+++ include/chunk.h
@@ -9,6 +9,10 @@ typedef enum {
     OP_NIL,
     OP_TRUE,
     OP_FALSE,
+    OP_POP,
+    OP_GET_GLOBAL,
+    OP_DEFINE_GLOBAL,
+    OP_SET_GLOBAL,
     OP_EQUAL,
     OP_GREATER,
     OP_LESS,
@@ -17,6 +21,7 @@ typedef enum {
     OP_MULTIPLY,
     OP_DIVIDE,
     OP_NEGATE,
+    OP_PRINT,
     OP_NOT,
     OP_RETURN,
 } Opcode;
